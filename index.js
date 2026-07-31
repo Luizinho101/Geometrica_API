92e57572afe7b31abe640ee64c2c8f55be3a39aa
Luizinho101 <luiz.antonio.da.silva777@gmail.com>
2026-07-30 23:42:28 -0300

    fix: ajustar vercel.json e rotas do express

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.use(express.json());
 
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/api/hello', (req, res) => {
@@ -12,12 +13,25 @@ app.get('/api/hello', (req, res) => {
 
 app.post('/api/area/quadrado', (req, res) => {
   const { lado } = req.body;
+
+  if (!lado || typeof lado !== 'number' || lado <= 0) {
+    return res.status(400).json({
+      error: "O campo 'lado' é obrigatório e deve ser um número positivo."
+    });
+  }
+
   const areaQuadrado = lado * lado;
 
-  res.status(200).json({
+  return res.status(200).json({
     resultado: areaQuadrado
   });
 });
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 
 if (process.env.NODE_ENV !== 'production') {
   const PORT = process.env.PORT || 3000;
