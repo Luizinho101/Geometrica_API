9cde66fab041debe93721c7ea57abf59b093e2f9
Luizinho101 <luiz.antonio.da.silva777@gmail.com>
2026-07-30 23:20:51 -0300

    feat: criar documentação da api com html

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.use(express.static('public'));
+
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Olá, Mundo!' });
 });
@@ -16,13 +18,14 @@ app.post('/api/area/quadrado', (req, res) => {
   const areaQuadrado = lado * lado;
 
 
-  res.status(201).json({
+  res.status(200).json({
     resultado: areaQuadrado
   });
 });
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
+  console.log(`Documentação na raiz: http://localhost:${PORT}/`);
   console.log(`http://localhost:${PORT}/api/hello`);
   console.log(`http://localhost:${PORT}/api/area/quadrado`);
 });
