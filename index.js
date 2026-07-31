9a9c9bd5d370ce4abd85be6335cba29a8d1c0d09
Luizinho101 <luiz.antonio.da.silva777@gmail.com>
2026-07-30 23:29:24 -0300

    feat: configurar projeto para deploy na vercel

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
 app.use(express.json());
-
 app.use(express.static('public'));
 
 app.get('/api/hello', (req, res) => {
@@ -12,20 +9,20 @@ app.get('/api/hello', (req, res) => {
 });
 
 app.post('/api/area/quadrado', (req, res) => {
-
   const { lado } = req.body;
-   
   const areaQuadrado = lado * lado;
-
 
   res.status(200).json({
     resultado: areaQuadrado
   });
 });
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-  console.log(`Documentação na raiz: http://localhost:${PORT}/`);
-  console.log(`http://localhost:${PORT}/api/hello`);
-  console.log(`http://localhost:${PORT}/api/area/quadrado`);
-});
+
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando localmente na porta ${PORT}`);
+  });
+}
+
+module.exports = app;
